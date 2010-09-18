15f19b2d57642ac30c9564b03020c72b582122a9
Colin Jones <trptcolin@gmail.com>
2010-09-18 13:17:40 -0500

    Add uninstall task.

--- src/leiningen/plugin.clj
+++ src/leiningen/plugin.clj
@@ -13,15 +13,21 @@
 
 (def plugins-path (file (home-dir) "plugins"))
 
+(defn plugin-standalone-filename [group name version]
+  (str (if group (str group "-") nil) name "-" version ".jar"))
+
+(defn extract-name-and-group [project-name]
+  ((juxt name namespace) (symbol project-name)))
+
 (defn plugin-install [project-name version]
   (install project-name version)
-  (let [[name group] ((juxt name namespace) (symbol project-name))
+  (let [[name group] (extract-name-and-group project-name)
         temp-project (format "/tmp/lein-%s" (java.util.UUID/randomUUID))
         jarfile (-> (local-repo-path name (or group name) version)
                     (.replace "$HOME" (System/getenv "HOME")))
         _ (extract-jar (file jarfile) temp-project)
         project (read-project (format "%s/project.clj" temp-project))
-        standalone-filename (get-default-uberjar-name project)]
+        standalone-filename (plugin-standalone-filename group name version)]
     (deps project)
     (with-open [out (-> (str plugins-path "/" standalone-filename)
                         (FileOutputStream.)
@@ -32,6 +38,10 @@
         (write-components deps out)))
     (println "Created" standalone-filename)))
 
+(defn plugin-uninstall [project-name version]
+  (let [[name group] (extract-name-and-group project-name)]
+    (.delete (file plugins-path (plugin-standalone-filename group name version)))))
+
 (defn plugin-help []
   (println "Plugin tasks available:
 
@@ -40,6 +50,9 @@
                   Syntax: lein plugin install GROUP/ARTIFACT-ID VERSION
                     You can use the same syntax here as when listing Leiningen
                     dependencies.
+
+  uninstall       Delete the plugin jarfile
+                    Syntax: lein plugin uninstall GROUP/ARTIFACT-ID
 
   help            Show this screen
 "))
@@ -51,5 +64,6 @@
   ([subtask project-name version]
     (case subtask
       "install" (plugin-install project-name version)
+      "uninstall" (plugin-uninstall project-name version)
       (plugin-help))))
 
