b6af55686dd5a0eae7312b8004fdf8f534139c7a
Colin Jones <trptcolin@gmail.com>
2010-09-18 13:44:44 -0500

    Update name in project.clj and remove leiningen as explicit dependency.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject lein-install-global-plugin "0.0.1-SNAPSHOT"
+(defproject lein-plugin "0.0.1-SNAPSHOT"
   :description "Install a plugin for Leiningen to be used on any project"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[leiningen "1.3.1"]])
+                 [org.clojure/clojure-contrib "1.2.0"]])
