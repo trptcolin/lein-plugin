16d8effe4a3de24c7fb8be40b3c7b82e5465e224
Colin Jones <trptcolin@gmail.com>
2010-09-18 13:54:54 -0500

    Remove unnecessary Clojure deps since we're in the lein classloader already.

--- project.clj
+++ project.clj
@@ -1,4 +1,2 @@
 (defproject lein-plugin "0.0.1"
-  :description "Install a plugin for Leiningen to be used on any project"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :description "Install a plugin for Leiningen to be used on any project")
